4466c361610afa0bee659e0234f269b923092b47
alexanderasche <alexander.asche@gmail.com>
2019-01-10 09:18:34 -0500

    init

---
+++ browserSync.js
@@ -0,0 +1,8 @@
+const browserSync = require("browser-sync");
+const BASE_URL = "https://pawsforacause.myshopify.com.myshopify.com";
+const PREVIEW_QUERY = "?preview_theme_id=44007161954";
+
+browserSync({
+  proxy: `${BASE_URL}${PREVIEW_QUERY}`,
+  files: "browserUpdate.js"
+});
