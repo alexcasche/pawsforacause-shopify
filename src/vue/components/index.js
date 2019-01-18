dd62e77333fefacd28a1ea376b22efec0924b601
alexanderasche <alexander.asche@gmail.com>
2019-01-18 18:23:32 -0500

    sidemenu open close function

--- src/vue/components/index.js
+++ src/vue/components/index.js
@@ -1,11 +1,11 @@
-import Header from './header/Header.vue'
-import MiniCart from './mini-cart/MiniCart.vue'
-import MobileMenu from './mobile-menu/MobileMenu.vue'
-import Portal from './portal/Portal.vue'
+// import Header from './header/Header.vue'
+// import MiniCart from './mini-cart/MiniCart.vue'
+// import MobileMenu from './mobile-menu/MobileMenu.vue'
+// import Portal from './portal/Portal.vue'
 
-export default {
-  'vue-header': Header,
-  'vue-mini-cart': MiniCart,
-  'vue-mobile-menu': MobileMenu,
-  'vue-portal': Portal,
-}
+// export default {
+//   'vue-header': Header,
+//   'vue-mini-cart': MiniCart,
+//   'vue-mobile-menu': MobileMenu,
+//   'vue-portal': Portal,
+// }
