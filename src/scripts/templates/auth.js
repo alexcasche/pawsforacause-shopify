a8d94270cf77c0762500c512920085569566ecc6
alexanderasche <alexander.asche@gmail.com>
2019-01-23 20:33:18 -0500

    account forms complete

--- src/scripts/templates/auth.js
+++ src/scripts/templates/auth.js
@@ -4,6 +4,13 @@ if(window.location.href.indexOf("account/login") > -1) {
   const loginForm =  document.getElementById("loginForm");
   const recoverForm = document.getElementById("recoverForm");
   const heading = document.getElementById("loginHeading");
+
+  function hideErrors() {
+    const errors = document.querySelectorAll(".c-auth__errors > .errors")
+    if(errors) {
+      errors.forEach(error => error.parentNode.removeChild(error));
+    }
+  }
   function showLogin() {
     heading.textContent = "Welcome Back!";
     loginForm.classList.remove("u-hidden");
@@ -18,8 +25,14 @@ if(window.location.href.indexOf("account/login") > -1) {
     showRecover()
   }
   const loginTrigger = document.querySelector(".trigger-login-form");
-  loginTrigger.addEventListener("click", showLogin);
+  loginTrigger.addEventListener("click", () => {
+    hideErrors();
+    showLogin();
+  });
   const recoverTrigger = document.querySelector(".trigger-recover-form");
-  recoverTrigger.addEventListener("click", showRecover);
+  recoverTrigger.addEventListener("click", () => {
+    hideErrors()
+    showRecover();
+  });
 }
 
