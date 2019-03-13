c24df9a1ae1ecb7841333f7d7d679ae8c2d66d27
alexanderasche <alexander.asche@gmail.com>
2019-03-13 18:19:24 -0400

    prefooter share done

--- src/scripts/snippets/part-share.js
+++ src/scripts/snippets/part-share.js
@@ -10,11 +10,53 @@ const windowOptions = `
   left=150
 `
 
+const facebookBtns = document.querySelectorAll('.c-share__link--facebook');
+facebookBtns.forEach(btn => btn.addEventListener('click', facebookShare));
+
+function facebookShare() {
+  const { url } = getDataset(event.target)
+  const share_url = `https://www.facebook.com/sharer/sharer.php?u=${url}`
+  window.open(share_url, '_blank', windowOptions)
+}
+
+
 const twitterBtns = document.querySelectorAll('.c-share__link--twitter');
 twitterBtns.forEach(btn => btn.addEventListener('click', twitterShare));
 
 function twitterShare() {
-  const { text, url } = event.target.dataset;
-  const share_url = `https://twitter.com/intent/tweet?url=${encodeURI(url)}&text=${encodeURI(text)}&via=PawsForACause`
+  const { text, url } = getDataset(event.target)
+  const share_url = `https://twitter.com/intent/tweet?url=${url}&text=${encodeURIComponent(text)} ${encodeURIComponent(url)}&via=PawsForACause`
   window.open(share_url, '_blank', windowOptions)
+}
+
+
+const pinterestBtns = document.querySelectorAll('.c-share__link--pinterest');
+pinterestBtns.forEach(btn => btn.addEventListener('click', pinterestShare));
+
+function pinterestShare() {
+  const { text, url, image } = getDataset(event.target)
+  const share_url = `https://pinterest.com/pin/create/button/?url=${url}&media=${encodeURIComponent(image)}&description=${encodeURIComponent(text)}`
+  window.open(share_url, '_blank', windowOptions)
+}
+
+
+const emailBtns = document.querySelectorAll('.c-share__link--email');
+emailBtns.forEach(btn => btn.addEventListener('click', emailShare));
+
+function emailShare() {
+  const { subject, url } = getDataset(event.target)
+  const body = `\n\n--\n\n${url}`
+  const formattedSubject = subject.toLowerCase()
+    .split(' ')
+    .map((s) => s.charAt(0).toUpperCase() + s.substring(1))
+    .join(' ');
+  window.location.href=`mailto:?subject=${encodeURIComponent(formattedSubject)}&body=${encodeURIComponent(body)}`
+}
+
+function getDataset(target) {
+  if(target.tagName !== 'a') {
+    return target.parentNode.dataset
+  } else {
+    return target.dataset
+  }
 }
