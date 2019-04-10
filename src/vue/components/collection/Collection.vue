a3342b7541f77742c9fa3fe59e66af751fed2626
alexanderasche <alexander.asche@gmail.com>
2019-04-09 22:31:46 -0400

    collection json in vue

---
+++ src/vue/components/collection/Collection.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>HELLO WORLD</div>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  props: {
+    collection: {
+      type: String,
+      default: "all-products"
+    }
+  },
+  async mounted() {
+    const collectionJSON = await axios.get(`/collections/${this.collection}-json`)
+    console.log(collectionJSON)
+  }
+}
+</script>
--- src/vue/components/index.js
+++ src/vue/components/index.js
@@ -1,11 +1,13 @@
 import Cart from './cart/Cart.vue'
 import CartAccount from './cart/CartAccount.vue'
+import Collection from './collection/Collection.vue'
 import Modal from './modal/Modal.vue'
 import ProductForm from './product/ProductForm.vue'
 
 export default {
-  'vue-cart-account': CartAccount,
   'vue-cart': Cart,
+  'vue-cart-account': CartAccount,
+  'vue-collection': Collection,
   'vue-modal': Modal,
   'vue-product-form': ProductForm
 }
